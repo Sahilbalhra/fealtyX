bde3f1443a7b55c5529677e1a5930fe945bfe7f4
Sahilbalhra <sahilbalhra0@gmail.com>
2025-03-25 23:13:40 +0530

    fixed logout navigation path

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -29,7 +29,7 @@ export default function Dashboard() {
     useStore.getState().setUser(null);
     toast.success("Logged out successfully !");
 
-    router.push("/auth/login");
+    router.push("/login");
   };
 
   if (!user) return null;
